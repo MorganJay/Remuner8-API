8de020176ad45bcba6970457fea602474feda0b2
James Morgan <james.morgan@thebulb.africa>
2021-01-29 13:44:14 +0000

    Remuner8.sql edited online with Bitbucket

--- Remuner8.sql
+++ Remuner8.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE Remuner8;
-USE Remuner8;
+USE db_payrollapp;
  CREATE TABLE Passwords(
                 email VARCHAR(50) NOT NULL UNIQUE PRIMARY KEY,
                 password VARCHAR(32) NOT NULL
@@ -27,11 +26,11 @@ CREATE TABLE Bonuses(
                departmentId INT  NOT NULL,
                bonusName DECIMAL(19,4) NOT NULL,
                amount DECIMAL(19,4) NOT NULL,
-               FOREIGN KEY (departmentId) REFERENCES Department(departmentId),
-               FOREIGN KEY (jobDescriptionId) REFERENCES JobDescription(jobDescriptionId)
+               FOREIGN KEY (departmentId) REFERENCES Departments(departmentId),
+               FOREIGN KEY (jobDescriptionId) REFERENCES JobDescriptions(jobDescriptionId)
 );
 
- CREATE TABLE SystemDefault(
+ CREATE TABLE SystemDefaults(
               companyName VARCHAR(30) NOT NULL,
               address VARCHAR(200) NOT NULL,
               email  VARCHAR(50) NOT NULL,
@@ -48,7 +47,7 @@ CREATE TABLE Bonuses(
              role VARCHAR(30) NOT NULL UNIQUE
  );
  CREATE TABLE Banks(
-			bankCode VARCHAR(10) PRIMARY KEY  NOT NULL,
+			bankCode VARCHAR(10) PRIMARY KEY NOT NULL,
             bankName VARCHAR(50) NOT NULL UNIQUE
 );
 CREATE TABLE EmploymentType(
@@ -58,6 +57,7 @@ CREATE TABLE EmploymentType(
  
  CREATE TABLE EmployeeBiodata (
 	        employeeId VARCHAR(10) PRIMARY KEY NOT NULL, 
+            avatar MEDIUMBLOB,
 			firstName VARCHAR(50) NOT NULL,
 			lastName VARCHAR(50) NOT NULL, 
             otherName VARCHAR(50) NOT NULL,
@@ -74,27 +74,27 @@ CREATE TABLE EmploymentType(
             dateEmployed DATE NOT NULL,
             otherAllowances DECIMAL(10, 4),
             grossSalary DECIMAL(19,4) NOT NULL, 
-            bankCode INT NOT NULL,
+            bankCode VARCHAR(10) NOT NULL,
             accountNumber VARCHAR(10) NOT NULL,
             FOREIGN KEY (departmentId) REFERENCES Departments(departmentId),
             FOREIGN KEY (jobDescriptionId) REFERENCES JobDescriptions(jobDescriptionId),
 			FOREIGN KEY (bankCode) REFERENCES Banks(bankCode),
-            FOREIGN KEY (emailAddress) REFERENCES Passowords(password)
+            FOREIGN KEY (emailAddress) REFERENCES Passwords(email)
 );
 CREATE TABLE StatutoryDeductions(
               statutoryTypeId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-              staffId INT NOT NULL,
+              employeeId VARCHAR(10) NOT NULL,
               amount1 DECIMAL(19,4) NOT NULL,
               amount2 DECIMAL(19,4) NOT NULL,
               pfaCode VARCHAR(10) NOT NULL ,
               pfaAccountNumber VARCHAR(10) NOT NULL,
               pfaAccountNumber1 VARCHAR (10) NOT NULL,
               description TEXT NOT NULL,
-              FOREIGN KEY (staffId) REFERENCES EmployeeBiodata(employeeId),
+              FOREIGN KEY (employeeId) REFERENCES EmployeeBiodata(employeeId),
               FOREIGN KEY (pfaCode) REFERENCES PensionFundAdministration(pfaCode)
 );
 CREATE TABLE PayrollTransactions(
-			staffId INT NOT NULL,
+			employeeId VARCHAR(10) NOT NULL,
 			transactionId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 			transactionDateTime DATETIME NOT NULL,
 			deduction BOOL NOT NULL,
@@ -102,10 +102,10 @@ CREATE TABLE PayrollTransactions(
 			rate DECIMAL(3,2) NOT NULL,
 			balance DECIMAL(15,2) NOT NULL,
 			statutory BOOL NOT NULL,
-			FOREIGN KEY (staffId) REFERENCES EmployeeBiodata(employeeId)
+			FOREIGN KEY (employeeId) REFERENCES EmployeeBiodata(employeeId)
 );
 CREATE TABLE TimeSheet(
-			employeeId INT NOT NULL,
+			employeeId VARCHAR(10) NOT NULL,
 			date DATE NOT NULL,
 			timeIn TIME ,
 			timeOut TIME ,
@@ -113,10 +113,8 @@ CREATE TABLE TimeSheet(
 			FOREIGN KEY (employeeId) REFERENCES EmployeeBiodata(employeeId)
  );
  CREATE TABLE Taxes(
-	        employeeId INT NOT NULL UNIQUE,
+	        employeeId VARCHAR(10) NOT NULL UNIQUE,
             PAYE DECIMAL(19,4),
             pension DECIMAL(19,4),
             FOREIGN KEY (employeeId) REFERENCES EmployeeBiodata(employeeId) 
 );
-
-
