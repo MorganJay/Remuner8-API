c1c87af94f9a72640d0611dd23c9bd3dea57e135
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-03-06 13:48:57 +0100

    more butchered stuff

---
+++ API/Models/PayrollAdditionItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace API.Models
+{
+    [Index(nameof(Assigneeid), Name = "IX_PayrollAdditionItems_AssigneeTableAssigneeid")]
+    [Index(nameof(CategoryId), Name = "IX_PayrollAdditionItems_PayrollCategoryCategoryId")]
+    public partial class PayrollAdditionItem
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required]
+        [Column("name")]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Column("amount", TypeName = "decimal(19, 4)")]
+        public decimal Amount { get; set; }
+        public int Assigneeid { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(Assigneeid))]
+        [InverseProperty(nameof(AssigneeTable.PayrollAdditionItems))]
+        public virtual AssigneeTable Assignee { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty(nameof(PayrollCategory.PayrollAdditionItems))]
+        public virtual PayrollCategory PayrollCategoryCategory { get; set; }
+    }
+}
