6906e65d59b3596bccc323dd74f34475c213a6e2
James Morgan <MorganJay@users.noreply.github.com>
2021-03-17 21:34:48 +0100

    Formatting

--- API/Repositories/EmployeeRepository.cs
+++ API/Repositories/EmployeeRepository.cs
@@ -1,7 +1,5 @@
 ﻿using API.Models;
 using Microsoft.EntityFrameworkCore;
-using Remuner8_Backend.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
--- API/Repositories/IPayrollDeductionRepository.cs
+++ API/Repositories/IPayrollDeductionRepository.cs
@@ -1,7 +1,5 @@
 ﻿using API.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Repositories
@@ -9,10 +7,15 @@ namespace API.Repositories
     public interface IPayrollDeductionRepository
     {
         Task<IEnumerable<PayrollDeductionItem>> GetAllItemsAsync();
+
         Task<PayrollDeductionItem> GetItemAsync(int id);
+
         Task AddItemsAsync(PayrollDeductionItem payrollDeductionItem);
+
         void EditItems(PayrollDeductionItem payrollDeductionItem);
+
         Task RemoveItemAsync(int id);
+
         Task<bool> SaveChangesAsync();
     }
 }
