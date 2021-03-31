050649ad6f544ed744902e92fc0d74c02795995e
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-03-31 13:44:35 +0100

    Employment Type API in progress

---
+++ API/Dtos/EmploymentTypeCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class EmploymentTypeCreateDto
+    {
+        [Key]
+        [Column("employmentTypeId")]
+        public int EmploymentTypeId { get; set; }
+        [Required]
+        [Column("employmentName")]
+        [StringLength(50)]
+        public string EmploymentName { get; set; }
+    }
+}
