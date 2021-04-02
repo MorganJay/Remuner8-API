83556caae3e389bfc9572d12b13704faef50285a
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-02 16:02:56 +0100

    Update to IPayrollCategoryRepository interface

--- API/Repositories/IPayrollCategoryRepository.cs
+++ API/Repositories/IPayrollCategoryRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Repositories
 {
-    interface IPayrollCategoryRepository
+    public interface IPayrollCategoryRepository
     {
+        Task<IEnumerable<PayrollCategory>> GetAllCategoriesAsync();
+        Task<PayrollCategory> GetCategoryByIdAsync(int id);
+        Task CreateCategoryAsync(PayrollCategory payrollCategory);
+        void EditCategory(PayrollCategory payrollCategory);
+        void DeleteCategory(int id);
+        Task<bool> SaveAsync();
     }
 }
