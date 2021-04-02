b696cdd1328d6308a2d17ad98c143cd7a7d4af72
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-02 14:39:37 +0100

    Working on Payroll Category Repositories

--- API/Profiles/AutomapperProfile.cs
+++ API/Profiles/AutomapperProfile.cs
@@ -34,6 +34,9 @@ namespace API
             CreateMap<Request, RequestCreateDto>().ReverseMap();
             CreateMap<PayrollRate, PayrollRateCreateDto>().ReverseMap();
             CreateMap<PayrollRate, PayrollRateReadDto>().ReverseMap();
+            CreateMap<PayrollCategory, PayrollCategoryReadDto>().ReverseMap();
+            CreateMap<PayrollCategory, PayrollRateCreateDto>().ReverseMap();
+
         }
     }
 }
---
+++ API/Repositories/IPayrollCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repositories
+{
+    public interface IPayrollCategoryRepository
+    {
+        Task<IEnumerable<PayrollCategory>> GetAllCategoriesAsync();
+        Task<PayrollCategory> GetCategoryByIdAsync(int id);
+        Task CreateCategoryAsync(PayrollCategory payrollCategory);
+        void EditCategory(PayrollCategory payrollCategory);
+        void DeleteCategory(int id);
+        Task<bool> SaveAsync();
+    }
+}
