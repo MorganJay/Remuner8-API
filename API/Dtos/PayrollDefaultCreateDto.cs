b292b2e9d1d53ab7a22bc93668c625c9e01f0d1f
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-03 15:54:46 +0100

    Payroll default Controller completed

--- API/Dtos/PayrollDefaultCreateDto.cs
+++ API/Dtos/PayrollDefaultCreateDto.cs
@@ -7,23 +7,36 @@ using System.Threading.Tasks;
 
 namespace API.Dtos
 {
-    public class PayrollDefaultsReadDto
+    public class PayrollDefaultCreateDto
     {
+        [Column(TypeName = "date")]
         public DateTime MinimumSalaryPeriod { get; set; }
+        [Column(TypeName = "date")]
         public DateTime MaximumSalaryPeriod { get; set; }
         public int MaxWorkingDays { get; set; }
         public int MaxOvertimeHours { get; set; }
+        [Column(TypeName = "decimal(5, 2)")]
         public decimal MinTaxPercentage { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal NonTaxableIncome { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal FirstAnnualTaxableIncome { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal SecondAnnualTaxableIncome { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal ThirdAnnualTaxableIncome { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal FourthAnnualTaxableIncome { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal FifthAnnualTaxableIncome { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal SixthAnnualTaxableIncome { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Tax { get; set; }
+        [StringLength(255)]
         public string Office { get; set; }
+        [StringLength(255)]
         public string Address { get; set; }
-        public int Id { get; set; }
     }
 }
