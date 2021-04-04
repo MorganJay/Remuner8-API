39233b392f66bc85033ce8756f54dbb18f35bf3f
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-04 10:36:15 +0100

    Jwt Authentication Signup and SignIn

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -18,10 +18,14 @@ namespace API.Controllers
     {
         private readonly UserManager<AppIdentityUser> _userManager;
         private readonly IMapper _mapper;
+        private readonly RoleManager<AppIdentityRole> _roleManager;
 
-        public AuthController(UserManager<AppIdentityUser> userManager, IMapper mapper)
+        public AuthController(UserManager<AppIdentityUser> userManager, 
+            RoleManager<AppIdentityRole> roleManager, 
+            IMapper mapper)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _mapper = mapper;
         }
 
@@ -38,6 +42,63 @@ namespace API.Controllers
             }
 
             return Problem(userCreateResult.Errors.First().Description, null, 500);
+        }
+
+        [HttpPost("SignIn")]
+        public async Task<IActionResult> SignIn(UserLoginResource userLoginResource)
+        {
+            var user = _userManager.Users.SingleOrDefault(u => u.UserName == userLoginResource.Email);
+            if (user is null)
+            {
+                return NotFound("User not found");
+            }
+
+            var userSigninResult = await _userManager.CheckPasswordAsync(user, userLoginResource.Password);
+
+            if (userSigninResult)
+            {
+                return Ok();
+            }
+
+            return BadRequest("Email or password incorrect.");
+        }
+
+        [HttpPost("Roles")]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            if (string.IsNullOrWhiteSpace(roleName))
+            {
+                return BadRequest("Role name should be provided.");
+            }
+
+            var newRole = new AppIdentityRole
+            {
+                Name = roleName
+            };
+
+            var roleResult = await _roleManager.CreateAsync(newRole);
+
+            if (roleResult.Succeeded)
+            {
+                return Ok();
+            }
+
+            return Problem(roleResult.Errors.First().Description, null, 500);
+        }
+        
+        [HttpPost("User/{userEmail}/Role")]
+        public async Task<IActionResult> AddUserToRole(string userEmail, [FromBody] string roleName)
+        {
+            var user = _userManager.Users.SingleOrDefault(u => u.UserName == userEmail);
+
+            var result = await _userManager.AddToRoleAsync(user, roleName);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return Problem(result.Errors.First().Description, null, 500);
         }
     }
 }
