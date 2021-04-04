3f9b92624a04683f5d9039b6fb40fa2143697a1b
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-04 11:20:20 +0100

    Reverting DbConext in startup

--- API/Settings/JwtSettings.cs
+++ API/Settings/JwtSettings.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace API.Settings
 {
-    public class JwtConfig
+    public class JwtSettings
     { 
         public string Secret { get; set; }
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,6 @@
 using API.Models;
 using API.Repositories;
 using API.Security;
-using API.Settings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -10,11 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 using System;
-using System.Text;
 
 namespace API
 {
@@ -56,7 +52,7 @@ namespace API
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1d);
                 options.Lockout.MaxFailedAccessAttempts = 5;
             })
-                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddEntityFrameworkStores<Remuner8Context>()
                 .AddDefaultTokenProviders();
 
             services.ConfigureApplicationCookie(Options =>
@@ -64,32 +60,6 @@ namespace API
                 Options.LoginPath = "/Security/SignIn";
                 Options.AccessDeniedPath = "/Security/AccessDenied";
             });
-
-            // within this section we are configuring the authentication and setting the default scheme
-            services.AddAuthentication(options => {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(jwt => {
-                var key = Encoding.ASCII.GetBytes(Configuration["JwtConfig:Secret"]);
-
-                jwt.SaveToken = true;
-                jwt.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true, // this will validate the 3rd part of the jwt token using the secret that we added in the appsettings and verify we have generated the jwt token
-        IssuerSigningKey = new SymmetricSecurityKey(key), // Add the secret key to our Jwt encryption
-        ValidateIssuer = false,
-                    ValidateAudience = false,
-                    RequireExpirationTime = false,
-                    ValidateLifetime = true
-                };
-            });
-
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //                .AddEntityFrameworkStores<AppIdentityDbContext>();
-
-            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
