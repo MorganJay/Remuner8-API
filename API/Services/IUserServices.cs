57ea04fb9e0daeebb0ede454b596f21286f685dd
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-16 03:50:39 +0100

    Password reset branch files created

---
+++ API/Services/IUserServices.cs
@@ -0,0 +1,21 @@
+﻿using API.Authentication;
+using API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public interface IUserServices
+    {
+        Task<RegistrationResponse> RegisterUserAsync(RegisterDto model);
+        Task<RegistrationResponse> LoginUserAsync(LoginDto model);
+        Task<RegistrationResponse> ConfirmEmailAsync(string userId, string token);
+        Task<RegistrationResponse> ForgetPasswordAsync(string email);
+
+
+    }
+
+
+}
---
+++ API/Services/UserServices.cs
@@ -0,0 +1,17 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class UserServices
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private IConfiguration _configuration;
+        private IMailService _mailService
+    }
+}
