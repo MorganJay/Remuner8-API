4093d2c2867ef241672838bb87a4939ddcfa3060
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-16 04:37:30 +0100

    Branch merged with Remote Register Branch

---
+++ API/Repositories/IPasswordRecoveryRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repositories
+{
+    public interface IPasswordRecoveryRepository
+    {
+        Task<RegistrationResponse> ForgetPasswordAsync(string email);
+    }
+}
---
+++ API/Repositories/PasswordRecoveryRepository.cs
@@ -0,0 +1,43 @@
+﻿using API.Authentication;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Repositories
+{
+    public class PasswordRecoveryRepository : IPasswordRecoveryRepository
+    {
+         private UserManager<ApplicationUser> _userManager;
+         private IConfiguration _configuration;
+
+        public PasswordRecoveryRepository(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<RegistrationResponse> ForgetPasswordAsync(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return new RegistrationResponse
+                {
+                    Message = "No user associated with email",
+                    Success = false
+                };
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var encodedToken = Encoding.UTF8.GetBytes(token);
+            var validToken = WebEncoders.Base64UrlEncode(encodedToken);
+
+        }
+    }
+}
