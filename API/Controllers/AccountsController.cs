df039cba1a5f89df1b9a5a7cb5d3c2ef240b94ef
Matthew Igbo <mattew.igbo@thebulb.africa>
2021-04-17 22:41:23 +0100

    Cleaned up Accounts Controller by moving methods into UserService class and injecting them into the Accounts Controller constructor through the IUserService Interface.

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -2,19 +2,14 @@
 using API.Dtos;
 using API.Models;
 using API.Repositories;
-using API.Settings;
+using API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -25,20 +20,18 @@ namespace API.Controllers
     [ApiController]
     public class AccountsController : ControllerBase
     {
+        private readonly IUserService _userService;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IMailServiceRepository _mailService;
-        private readonly JwtSettings _jwtSettings; 
-        private readonly TokenValidationParameters _tokenValidationParameters;
-        private readonly Remuner8Context _remuner8Context;
+        
 
-        public AccountsController(UserManager<ApplicationUser> userManager, IMailServiceRepository mailService, IOptionsMonitor<JwtSettings> jwtSettings,
-                TokenValidationParameters tokenValidationParameters, Remuner8Context remuner8Context)
+        public AccountsController(UserManager<ApplicationUser> userManager, IMailServiceRepository mailService,
+                IUserService userService)
         {
+            _userService = userService;
             _userManager = userManager;
             _mailService = mailService;
-            _jwtSettings = jwtSettings.CurrentValue;
-            _tokenValidationParameters = tokenValidationParameters;
-            _remuner8Context = remuner8Context;
+            
         }
 
         [HttpPost("Register")]
@@ -65,7 +58,7 @@ namespace API.Controllers
                 var isCreated = await _userManager.CreateAsync(newUser, model.Password);
                 if (isCreated.Succeeded)
                 {
-                    var jwtToken = await GenerateJwtToken(newUser);
+                    var jwtToken = await _userService.GenerateJwtToken(newUser);
                     return Ok(jwtToken);
 
                 }
@@ -99,7 +92,7 @@ namespace API.Controllers
                         if (verifyPassword == true)
                         {
                             await _mailService.SendEmailAsync(model.Email, "New login", "<h1>Hey!, new login to your account noticed</h1><p>New login to your account at " + DateTime.Now + "</p>");
-                            var jwtToken = await GenerateJwtToken(verifyEmail);
+                            var jwtToken = await _userService.GenerateJwtToken(verifyEmail);
                             return Ok(jwtToken);
                         }
                     }
@@ -146,64 +139,13 @@ namespace API.Controllers
             }
         }
 
-        private async Task<RegistrationResponse> GenerateJwtToken(IdentityUser user)
-        {
-            var jwtTokenHandler = new JwtSecurityTokenHandler();
-
-            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new[]
-                {
-                    new Claim("Id", user.Id),
-                    new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                    new Claim(JwtRegisteredClaimNames.Sub, user.Email),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            }),
-                Expires = DateTime.UtcNow.AddHours(6),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
-            var jwtToken = jwtTokenHandler.WriteToken(token);
-            var refreshToken = new RefreshToken()
-            {
-                JwtId = token.Id,
-                IsUsed = false,
-                UserId = user.Id,
-                AddedDate = DateTime.UtcNow,
-                ExpiryDate = DateTime.Now.AddYears(1),
-                IsRevoked = false,
-                Token = RandomString(25) + Guid.NewGuid()
-            };
-
-            await _remuner8Context.RefreshTokens.AddAsync(refreshToken);
-            await _remuner8Context.SaveChangesAsync();
-
-            return new RegistrationResponse()
-            {
-                Token = jwtToken,
-                Success = true,
-                RefreshToken = refreshToken.Token
-            };
-
-        }
-
-        public static string RandomString(int length)
-        {
-            var random = new Random();
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-            .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
 
         [HttpPost("RefreshToken")]
         public async Task<IActionResult> RefreshToken([FromBody] TokenRequest tokenRequest)
         {
             if (ModelState.IsValid)
             {
-                var res = await VerifyToken(tokenRequest);
+                var res = await _userService.VerifyToken(tokenRequest);
 
                 if (res == null)
                 {
@@ -227,118 +169,6 @@ namespace API.Controllers
                 Success = false
             });
 
-        }
-
-        private async Task<RegistrationResponse> VerifyToken(TokenRequest tokenRequest)
-        {
-            var jwtTokenHandler = new JwtSecurityTokenHandler();
-
-            try
-            {
-                // This validation function will make sure that the token meets the validation parameters
-                // and its an actual jwt token not just a random string
-                var principal = jwtTokenHandler.ValidateToken(tokenRequest.Token, _tokenValidationParameters, out var validatedToken);
-
-                // Now we need to check if the token has a valid security algorithm
-                if (validatedToken is JwtSecurityToken jwtSecurityToken)
-                {
-                    var result = jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase);
-
-                    if (result == false)
-                    {
-                        return null;
-                    }
-                }
-
-                // Will get the time stamp in unix time
-                var utcExpiryDate = long.Parse(principal.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Exp).Value);
-
-                // we convert the expiry date from seconds to the date
-                var expDate = UnixTimeStampToDateTime(utcExpiryDate);
-
-                if (expDate > DateTime.UtcNow)
-                {
-                    return new RegistrationResponse()
-                    {
-                        Errors = new List<string>() { "We cannot refresh this since the token has not expired" },
-                        Success = false
-                    };
-                }
-
-                // Check the token we got if its saved in the db
-                var storedRefreshToken = await _remuner8Context.RefreshTokens.FirstOrDefaultAsync(x => x.Token == tokenRequest.RefreshToken);
-
-                if (storedRefreshToken == null)
-                {
-                    return new RegistrationResponse()
-                    {
-                        Errors = new List<string>() { "refresh token doesnt exist" },
-                        Success = false
-                    };
-                }
-
-                // Check the date of the saved token if it has expired
-                if (DateTime.UtcNow > storedRefreshToken.ExpiryDate)
-                {
-                    return new RegistrationResponse()
-                    {
-                        Errors = new List<string>() { "token has expired, user needs to relogin" },
-                        Success = false
-                    };
-                }
-
-                // check if the refresh token has been used
-                if (storedRefreshToken.IsUsed)
-                {
-                    return new RegistrationResponse()
-                    {
-                        Errors = new List<string>() { "token has been used" },
-                        Success = false
-                    };
-                }
-
-                // Check if the token is revoked
-                if (storedRefreshToken.IsRevoked)
-                {
-                    return new RegistrationResponse()
-                    {
-                        Errors = new List<string>() { "token has been revoked" },
-                        Success = false
-                    };
-                }
-
-                // we are getting here the jwt token id
-                var jti = principal.Claims.SingleOrDefault(x => x.Type == JwtRegisteredClaimNames.Jti).Value;
-
-                // check the id that the recieved token has against the id saved in the db
-                if (storedRefreshToken.JwtId != jti)
-                {
-                    return new RegistrationResponse()
-                    {
-                        Errors = new List<string>() { "the token doenst mateched the saved token" },
-                        Success = false
-                    };
-                }
-
-                storedRefreshToken.IsUsed = true;
-                _remuner8Context.RefreshTokens.Update(storedRefreshToken);
-                await _remuner8Context.SaveChangesAsync();
-
-                var dbUser = await _userManager.FindByIdAsync(storedRefreshToken.UserId);
-                return await GenerateJwtToken(dbUser);
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        private static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
-        {
-            // Unix timestamp is seconds past epoch
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-            return dtDateTime;
         }
 
     }
---
+++ API/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using API.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public interface IUserService
+    {
+        Task<RegistrationResponse> GenerateJwtToken(IdentityUser user);
+        string RandomString(int length);
+        Task<RegistrationResponse> VerifyToken(TokenRequest tokenRequest);
+        DateTime UnixTimeStampToDateTime(double unixTimeStamp);
+    }
+}
